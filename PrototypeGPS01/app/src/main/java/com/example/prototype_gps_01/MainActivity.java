c974a91dad6c20277e4f2a12b1c6e9a9da30c826
mzamith-ufrrj <mzamith@ufrrj.br>
2024-05-29 17:07:34 -0300

    2024-05-29 update #1 - including GPS demo 01 - compass demo

---
+++ PrototypeGPS01/app/src/main/java/com/example/prototype_gps_01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.prototype_gps_01;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        
+    }//protected void onCreate(Bundle savedInstanceState) {
+}//public class MainActivity extends AppCompatActivity {
